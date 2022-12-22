bf7d6c4d7c2b7ce4fd1919054f63c356c511dc0b
Moritz <phis@mogobb.de>
2022-12-22 10:08:40 +0100

    Dragonriding

--- CrazyForMounts.lua
+++ CrazyForMounts.lua
@@ -14,6 +14,7 @@ BINDING_HEADER_CRAZYFORMOUNTS = addonName
 BINDING_NAME_CRAZYFORMOUNTS_SUMMON_RANDOM = "Summon random mount"
 BINDING_NAME_CRAZYFORMOUNTS_SUMMON_FLYING = "Summon random flying mount"
 BINDING_NAME_CRAZYFORMOUNTS_SUMMON_GROUND = "Summon random ground mount"
+BINDING_NAME_CRAZYFORMOUNTS_SUMMON_RIDING = "Summon random dragonriding mount"
 CrazyForMountsGlobals = {}
 
 -------------------------
@@ -26,6 +27,15 @@ local function getLength(tbl)
 		length = length + 1
 	end
 	return length
+end
+
+local function hasValue(tbl, val)
+	for _, v in ipairs(tbl) do
+		if v == val then
+			return true
+		end
+	end
+	return false	
 end
 
 local function addonPrint(str)
@@ -52,6 +62,34 @@ function phis.IsFlyableArea()
 	end
 	
 	return true
+end
+
+-- checks whether the player can actually use dragon riding
+function phis.IsDragonRidableArea()
+	-- check if player has (no) mounts for Dragonriding
+	local collectedDragonridingMounts = C_MountJournal.GetCollectedDragonridingMounts() 
+	if not collectedDragonridingMounts or getLength(collectedDragonridingMounts) == 0 then
+		return false
+	end
+	
+	-- check if in instance (https://wowpedia.fandom.com/wiki/API_IsInInstance)
+	if IsInInstance() then
+		return false
+	end
+	
+	-- check if in Dragon isles -> loop over parent map ids until id == 1978 or id == nil or id == 0
+	local mapID = C_Map.GetBestMapForUnit('player')
+	while mapID and mapID ~= 0 do
+		if mapID == 1978 then
+			return true
+		end
+		local mapInfo = C_Map.GetMapInfo(mapID)
+		if mapInfo then
+			mapID = mapInfo.parentMapID
+		end
+	end
+	
+	return false
 end
 
 -------------------------
@@ -137,8 +175,8 @@ local function updateList()
 			end
 			personalFavoriteIcons[v]:Hide()
 			
-			-- only show the icon again if the frame contains a pet whose petID is in the set of personal favorites
-			if personalMountDB['ground'][mountID] ~= nil or personalMountDB['flying'][mountID] ~= nil then
+			-- only show the icon again if the frame contains a mount whose mountID is in the set of personal favorites
+			if personalMountDB['ground'][mountID] ~= nil or personalMountDB['flying'][mountID] ~= nil or personalMountDB['riding'][mountID] ~= nil then
 				personalFavoriteIcons[v]:Show()
 			end
 		end
@@ -160,14 +198,22 @@ local function summonRandom(mountType)
 	
 	-- if mountType is nil select the appropriate mount table
 	-- else select the table corresponding to the given argument
+	-- prioritizes Dragonriding mounts over flying mounts
 	local canFly = false
+	local canRide = false
 	if mountType == nil then
 		canFly = phis.IsFlyableArea()
+		canRide = phis.IsDragonRidableArea()
 	elseif mountType == 'flying' then
 		canFly = true
+	elseif mountType == 'riding' then
+		canRide = true
 	end
 	
-	if canFly then
+	if canRide then
+		tmpCount = (personalMountCount.riding or 0)
+		tmpMountDB = (personalMountDB.riding or {})
+	elseif canFly then
 		tmpCount = (personalMountCount.flying or 0)
 		tmpMountDB = (personalMountDB.flying or {})
 	else
@@ -184,15 +230,15 @@ local function summonRandom(mountType)
 		local mountID = tmpIDs[math.random(#tmpIDs)]
 		C_MountJournal.SummonByID(mountID)
 	else
-		addonPrint('No personal '..(canFly and 'flying' or 'ground')..' mounts set.')
+		addonPrint('No personal '..(canRide and 'Dragonriding' or (canFly and 'flying' or 'ground'))..' mounts set.')
 	end
 end
 -- add to globals for keybindings
 CrazyForMountsGlobals.summonRandom = summonRandom
 
 -- mountIDs are used as keys for more efficient lookup
-local function updateDB(mountID, flyable, addMount)
-	mountType = flyable and 'flying' or 'ground'
+local function updateDB(mountID, mountType, addMount)
+	-- mountType = flyable and 'flying' or 'ground'
 	if addMount and personalMountDB[mountType][mountID] == nil then
 		personalMountDB[mountType][mountID] = true
 		personalMountCount[mountType] = personalMountCount[mountType] + 1
@@ -206,11 +252,23 @@ local function initAddon()
 	--- SETUP VARIABLES ---
 	
 	if personalMountDB == nil then
-		personalMountDB = {ground = {}, flying = {}}
+		personalMountDB = {ground = {}, flying = {}, riding = {}}		
+	end
+	
+	if personalMountDB.ground == nil then
+		personalMountDB.ground = {}
+	end
+	
+	if personalMountDB.flying == nil then
+		personalMountDB.flying = {}
+	end
+	
+	if personalMountDB.riding == nil then
+		personalMountDB.riding = {}
 	end
 	
 	if personalMountCount == nil then
-		personalMountCount = {ground = 0, flying = 0}
+		personalMountCount = {ground = 0, flying = 0, riding = 0}
 		local playerName, playerRealm = UnitFullName('player')
 		local _, playerClass = UnitClass('player')
 		local _, _, _, classColor = GetClassColor(playerClass)
@@ -220,13 +278,19 @@ local function initAddon()
 	--- CREATE AND ATTACH FRAMES ---
 	local groundMountInset = createInset('groundMountInset', MountJournal, 100, 20, 'BOTTOMRIGHT', -7, 5, 'Ground: ', personalMountCount.ground)
 	local flyingMountInset = createInset('flyingMountInset', groundMountInset, 100, 20, 'LEFT', -110, 0, 'Flying: ', personalMountCount.flying)
+	local ridingMountInset = createInset('ridingMountInset', flyingMountInset, 120, 20, 'LEFT', -130, 0, 'Dragonriding: ', personalMountCount.riding)
+	
+	-- icons are from (all with CC0 license):
+	-- https://www.pngrepo.com/svg/307488/dragon-with-wings-monster-legend-myth
+	-- https://www.pngrepo.com/svg/37053/flying-dove-bird-shape
+	-- https://www.pngrepo.com/svg/140806/horse-jumping-silhouette
 	
 	local checkBoxGround = createCheckbox('CrazyForMountsCheckBoxGround', MountJournal.MountDisplay, 'TOPLEFT', MountJournal.MountDisplay, 'BOTTOMLEFT', 10, 52, 'Add this mount to your personal ground mounts', 'Interface\\Addons\\CrazyForMounts\\Icons\\horse')
 	checkBoxGround:SetScript('OnClick', function(self)
 		local checked = self:GetChecked()
 		PlaySound(checked and SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON or SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_OFF)
 		local mountID = MountJournal.selectedMountID
-		updateDB(mountID, false, checked)
+		updateDB(mountID, 'ground', checked)
 		updateList()
 		groundMountInset.content:SetText(personalMountCount.ground)
 	end)
@@ -236,21 +300,48 @@ local function initAddon()
 		local checked = self:GetChecked()
 		PlaySound(checked and SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON or SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_OFF)
 		local mountID = MountJournal.selectedMountID
-		updateDB(mountID, true, checked)
+		updateDB(mountID, 'flying', checked)
 		updateList()
 		flyingMountInset.content:SetText(personalMountCount.flying)
+	end)
+	
+	local checkBoxRiding = createCheckbox('CrazyForMountsCheckBoxRiding', MountJournal.MountDisplay, 'LEFT', checkBoxFlying, 'RIGHT', 10, 0, 'Add this mount to your personal Dragonriding mounts', 'Interface\\Addons\\CrazyForMounts\\Icons\\dragon')
+	checkBoxRiding:SetScript('OnClick', function(self)
+		local checked = self:GetChecked()
+		PlaySound(checked and SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON or SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_OFF)
+		local mountID = MountJournal.selectedMountID
+		updateDB(mountID, 'riding', checked)
+		updateList()
+		ridingMountInset.content:SetText(personalMountCount.riding)
 	end)
 	
 	hooksecurefunc('MountJournal_UpdateMountDisplay', function()
 		local mountID = MountJournal.selectedMountID
 		checkBoxGround:SetChecked(personalMountDB.ground[mountID] == true)
 		checkBoxFlying:SetChecked(personalMountDB.flying[mountID] == true)
+		checkBoxRiding:SetChecked(personalMountDB.riding[mountID] == true)
+		
 		local _, _, _, _, mountType = C_MountJournal.GetMountInfoExtraByID(mountID)
+		-- disable checking Dragonriding mounts as ground mounts (because they can only be used in the Dragon Isles)
+		checkBoxGround:Enable()
+		checkBoxGround:SetAlpha(1)
+		if hasValue(C_MountJournal.GetCollectedDragonridingMounts(), mountID) and locked then
+			checkBoxGround:SetAlpha(0.5)
+			checkBoxGround:Disable()
+		end
+		-- disable checking non-flying mounts as flying mounts
 		checkBoxFlying:Enable()
 		checkBoxFlying:SetAlpha(1)
 		if mountType ~= 248 and locked then
 			checkBoxFlying:SetAlpha(0.5)
 			checkBoxFlying:Disable()
+		end
+		-- disable non-Dragonriding mounts as Dragonriding mounts
+		checkBoxRiding:Enable()
+		checkBoxRiding:SetAlpha(1)
+		if not hasValue(C_MountJournal.GetCollectedDragonridingMounts(), mountID) and locked then
+			checkBoxRiding:SetAlpha(0.5)
+			checkBoxRiding:Disable()
 		end
 	end)
 	
@@ -259,6 +350,7 @@ local function initAddon()
 	
 	personalMountCount.ground = getLength(personalMountDB.ground)
 	personalMountCount.flying = getLength(personalMountDB.flying)
+	personalMountCount.riding = getLength(personalMountDB.riding)
 end
 
 -- checks if both the addon itself and the Blizzard Collections addon are loaded
@@ -285,19 +377,19 @@ SlashCmdList['CFM'] = function(msg)
 	local arg1, arg2 = strsplit(' ', msg)
 	if arg1:lower() == 'toggle' then
 		locked = not locked
-		addonPrint('Setting non-flying mounts as flying mounts is now '..(locked and 'locked' or 'unlocked')..'.')
+		addonPrint('Setting non-flying mounts as flying or Dragonriding mounts is now '..(locked and 'locked' or 'unlocked')..'.')
 		if MountJournal then
 			MountJournal_UpdateMountDisplay(true)
 		end
 	elseif arg1:lower() == 'lock' then
 		locked = true
-		addonPrint('Setting non-flying mounts as flying mounts is now locked.')
+		addonPrint('Setting non-flying mounts as flying or Dragonriding mounts is now locked.')
 		if MountJournal then
 			MountJournal_UpdateMountDisplay(true)
 		end
 	elseif arg1:lower() == 'unlock' then
 		locked = false
-		addonPrint('Setting non-flying mounts as flying mounts is now unlocked.')
+		addonPrint('Setting non-flying mounts as flying or Dragonriding mounts is now unlocked.')
 		if MountJournal then
 			MountJournal_UpdateMountDisplay(true)
 		end
@@ -305,6 +397,8 @@ SlashCmdList['CFM'] = function(msg)
 		summonRandom('flying')
 	elseif arg1:lower() == 'ground' then
 		summonRandom('ground')
+	elseif arg1:lower() == 'riding' then
+		summonRandom('riding')
 	else
 		summonRandom()
 	end
